be0a7f381780fb3b41c629fcac7edcf6aecff383
Gabriel Torelo <57268681+GabrielTorelo@users.noreply.github.com>
2022-06-28 00:03:46 -0300

    Trade API KEY

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,12 +3,12 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyADsnmMhWU6WLeGNG-vK95YUhdK0lWBiUM",
-  authDomain: "app-prog-dm2-4741b.firebaseapp.com",
-  projectId: "app-prog-dm2-4741b",
-  storageBucket: "app-prog-dm2-4741b.appspot.com",
-  messagingSenderId: "33888733809",
-  appId: "1:33888733809:web:1f5c46397b60aa808f29d0"
+  apiKey: "AIzaSyARjeXf0ycU3ds7tW4QShzNAee8tW-pO4o",
+  authDomain: "app-prog-dm2.firebaseapp.com",
+  projectId: "app-prog-dm2",
+  storageBucket: "app-prog-dm2.appspot.com",
+  messagingSenderId: "753745607433",
+  appId: "1:753745607433:web:56c0074996460f3fd771d8"
 };
 
   // Initialize Firebase
